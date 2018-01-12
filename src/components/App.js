a9bdd42053a60e587b85db71e29647108e967d07
mona87 <bellamy2727@yahoo.com>
2018-01-12 17:14:28 -0600

    save

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,25 @@ import { firebaseApp} from '../firebase';
 import AddTask from './AddTask';
 import TaskList from'./TaskList';
 import BottomNav from './BottomNav';
+import {Tabs, Tab} from 'material-ui/Tabs';
+// import Slider from 'material-ui/Slider';
+import SwipeableViews from 'react-swipeable-views';
 
 import CompleteTaskList from './CompleteTaskList';
 
 
 class App extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	      slideIndex: 0,
+	    };
+	}
+	handleChange = (value) => {
+	    this.setState({
+	      slideIndex: value,
+	    });
+	};
 	signOut(){
 		firebaseApp.auth().signOut();
 	}
@@ -27,16 +41,43 @@ class App extends Component {
 			  title="Tracker" 
 				showMenuIconButton={false}
 			  />
-			  			 <RaisedButton label="Sign Out"  
-			  backgroundColor='#212121'
-			  labelColor='#ffffff'
-			  style={{display: 'block', width: '200px', margin: '20px auto auto auto'}}
-			   onClick={() => this.signOut()}
-			   />
+			<Tabs
+			    onChange={this.handleChange}
+      			value={this.state.slideIndex}
+			 >
+			    <Tab label="Task List" value={0}  style={{backgroundColor: '#455A64'}}/>
+			    <Tab label="Completed" value={1}  style={{backgroundColor: '#455A64'}}/>
+			 </Tabs> 
+			 <SwipeableViews
+		          index={this.state.slideIndex}
+		          onChangeIndex={this.handleChange}
+		          style={{marginBottom: '50px'}}
+		        >
+			     <div>
 			  		<AddTask />
 			  		<TaskList />
+			  		<RaisedButton label="Sign Out"  
+					  backgroundColor='#212121'
+					  labelColor='#ffffff'
+					  style={{display: 'block', width: '200px', margin: '20px auto auto auto'}}
+					  onClick={() => this.signOut()}
+			   		/>
+			  	</div>
+				<div>
 			  		<h2>Completed Tasks</h2>
 			  		<CompleteTaskList />
+			  		<RaisedButton label="Sign Out"  
+					  backgroundColor='#212121'
+					  labelColor='#ffffff'
+					  style={{display: 'block', width: '200px', margin: '20px auto auto auto'}}
+					  onClick={() => this.signOut()}
+			   		/>
+				</div>
+				<div>			  			
+
+			   </div>
+			</SwipeableViews>
+	
 			   <BottomNav/>
 			 </MuiThemeProvider>
 		)
